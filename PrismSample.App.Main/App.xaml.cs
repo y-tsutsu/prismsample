b5f038f14e448637c74c759008051f38b1770acb
tsutsu <tsutsu0928@gmail.com>
2022-03-12 17:45:31 +0900

    PrismApplicationに変更

--- PrismSample.App.Main/App.xaml.cs
+++ PrismSample.App.Main/App.xaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
 
 namespace PrismSample.App.Main
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
     }
 }
